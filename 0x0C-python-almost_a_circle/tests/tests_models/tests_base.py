718b690b435dc936954fb1f3078992bd03ef7fc1
csarsound <csarsound@hotmail.com>
2022-06-14 15:58:10 -0500

    Base Class

--- 0x0C-python-almost_a_circle/tests/tests_models/tests_base.py
+++ 0x0C-python-almost_a_circle/tests/tests_models/tests_base.py
@@ -10,12 +10,11 @@ import unittest
 
 class TestBase_instantiation(unittest.TestCase):
      """Unittests for testing instatiation of the Base Class"""
-
      def _no_arg(self):
-          b1 = Base()
-          b2 = Base()
-          self.assertEqual(b1.id, b2.id - 1)
-
+    	     b1 = Base()
+	     b2 = Base()
+	     self.assertEqual(b1.id, b2.id - 1)
+	     
 if __name__ == "__main__":
-    unittest.main()
+    	unittest.main()
      
