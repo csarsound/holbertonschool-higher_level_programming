d03690786d1ce0f231aead59fecdbd2cfa3c0746
csarsound <csarsound@hotmail.com>
2022-08-16 18:50:21 -0500

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-if (argv.lenght === 2)
-{
-    console.log('No argument');
-} else if (argv.lenght === 3)
-{
-    console.log('argument found');
-} else if (argv.lenght > 3)
-{
-    console.log('Arguments found');
+if (argv.length === 2) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('argument found');
+} else if (argv.length > 3) {
+  console.log('Arguments found');
 }
