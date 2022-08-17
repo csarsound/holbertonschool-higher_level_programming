b10bb9a31a8d83e677612c09235054bf373e5071
csarsound <csarsound@hotmail.com>
2022-08-16 19:30:53 -0500

    Square

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,6 +4,6 @@ if (process.argv[2]) {
     for (let i = 0; i < x; i++) {
       console.log('C is fun');
     }
-  } else {
-    console.log('Missing number of occurrences');
-  }
+} else {
+  console.log('Missing number of occurrences');
+}
