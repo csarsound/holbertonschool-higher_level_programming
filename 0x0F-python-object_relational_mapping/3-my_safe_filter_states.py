84470f3c697c1a64412f252c66a1009dd48cb7c7
csarsound <csarsound@hotmail.com>
2022-09-01 15:22:13 -0500

    SafeFilter

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-Script that lists all `cities` from the database `hbtn_0e_4_usa`.
+Script that lists all values in the `states` table of `hbtn_0e_0_usa`
+where `name` matches the argument `state name searched`.
 Arguments:
     mysql username (str)
     mysql password (str)
     database name (str)
+    state name searched (str)
 """
 
 import sys
@@ -15,14 +17,14 @@ if __name__ == "__main__":
     mySQL_p = sys.argv[2]
     db_name = sys.argv[3]
 
+    searched_name = sys.argv[4]
+
     # By default, it will connect to localhost:3306
     db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
     cur = db.cursor()
 
-    cur.execute("SELECT c.id, c.name, s.name \
-                 FROM cities c INNER JOIN states s \
-                 ON c.state_id = s.id \
-                 ORDER BY c.id")
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id",
+                (searched_name, ))
     rows = cur.fetchall()
 
     for row in rows:
